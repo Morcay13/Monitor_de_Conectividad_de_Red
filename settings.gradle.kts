ddc7090eb73e0c05474fce3b0fd425975127e0ea
Morcay <jesus.delafuente.espinilla@iesjulianmarias.es>
2024-11-15 22:37:36 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Creación_de_un_Monitor_de_Conectividad_de_Red"
+include(":app")
+ 
